4f10303e4bbf1689a99203c8281c9a29f827f20f
unknown <rachaelvallee2019@gmail.com>
2026-08-29 16:46:42 -0500

    update hot collections

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -29,52 +29,46 @@ const HotCollections = () => {
     slidesToScroll: 1,
   };
   return (
-    <>
-      
-       
-      <section id="section-collections" className="no-bottom">
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-12">
-              <div className="text-center">
-                <h2>Hot Collections</h2>
-                <div className="small-border bg-color-2"></div>
-              </div>
-              < div classname="col-lg-12">
-                <Slider {...settings}>
-                {collections.map((collection) => (
-              <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" 
-              key={collection.id}
-              style={{ padding: "0 10px" }}
-              >
-                <div className="nft_coll">
-                  <div className="nft_wrap">
-                    <Link to="/item-details">
-                      <img src={collection.nftImage} className="lazy img-fluid" alt="" />
-                    </Link>
-                  </div>
-                  <div className="nft_coll_pp">
-                    <Link to="/author">
-                      <img className="lazy pp-coll" src={collection.authorImage} alt="" />
-                    </Link>
-                    <i className="fa fa-check"></i>
-                  </div>
-                  <div className="nft_coll_info">
-                    <Link to="/explore">
-                      <h4>{collection.title}</h4>
-                    </Link>
-                    <span>{collection.code}</span>
-                  </div>
-                </div>
-              </div>
-            ))}
-              </Slider>
-              </div>
+    <section id="section-collections" className="no-bottom">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-12">
+            <div className="text-center">
+              <h2>Hot Collections</h2>
+              <div className="small-border bg-color-2"></div>
             </div>
           </div>
+
+          <div className="col-lg-12">
+            <Slider {...settings}>
+              {collections.map((collection) => (
+                <div key={collection.id} style={{ padding: "0 10px" }}>
+                  <div className="nft_coll">
+                    <div className="nft_wrap">
+                      <Link to="/item-details">
+                        <img src={collection.nftImage} className="lazy img-fluid" alt="" />
+                      </Link>
+                    </div>
+                    <div className="nft_coll_pp">
+                      <Link to="/author">
+                        <img className="lazy pp-coll" src={collection.authorImage} alt="" />
+                      </Link>
+                      <i className="fa fa-check"></i>
+                    </div>
+                    <div className="nft_coll_info">
+                      <Link to="/explore">
+                        <h4>{collection.title}</h4>
+                      </Link>
+                      <span>{collection.code}</span>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </Slider>
+          </div>
         </div>
-      </section>
-    </>
+      </div>
+    </section>
   );
 };
 
