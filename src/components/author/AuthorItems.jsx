f41c77d608b5a6d6ac2e1377a3c9341451fe212c
unknown <rachaelvallee2019@gmail.com>
2026-09-03 15:51:16 -0500

    update href

--- src/components/author/AuthorItems.jsx
+++ src/components/author/AuthorItems.jsx
@@ -23,13 +23,13 @@ const AuthorItems = () => {
                       <button>Buy Now</button>
                       <div className="nft__item_share">
                         <h4>Share</h4>
-                        <a href="" target="_blank" rel="noreferrer">
+                        <a href="/about" target="_blank" rel="noreferrer">
                           <i className="fa fa-facebook fa-lg"></i>
                         </a>
                         <a href="" target="_blank" rel="noreferrer">
                           <i className="fa fa-twitter fa-lg"></i>
                         </a>
-                        <a href="">
+                        <a href="/contact" target="_blank" rel="noreferrer">
                           <i className="fa fa-envelope fa-lg"></i>
                         </a>
                       </div>
